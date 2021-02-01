5ae6128927ff272be601550b60332549ecc9237e
Helge Stenström <h.stenstrom@gmail.com>
2021-02-01 17:28:39 +0100

    put classes in a package, to have the same structure as for the extracted MVC branch.

--- src/main/java/mypackage/CoffeeMachine.java
+++ src/main/java/mypackage/CoffeeMachine.java
@@ -1,9 +1,11 @@
+package mypackage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
 
 public class CoffeeMachine {
