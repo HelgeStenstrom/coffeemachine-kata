1d5c2c959a0190f3e5437983f2735c5e2c890ff9
Helge Stenström <h.stenstrom@gmail.com>
2021-02-04 20:55:09 +0100

    Updated test from the Testing branch

--- src/test/java/mypackage/CoffeeMachineTest.java
+++ src/test/java/mypackage/CoffeeMachineTest.java
@@ -1,17 +1,45 @@
 package mypackage;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CoffeeMachineTest {
 
-    @Test
-    void quitting() {
+    private PrintStream originalOut;
+    private InputStream originalIn;
 
-        var output = CoffeeMachineRunner.runCoffeeMachine("q\n");
+    @BeforeEach
+    void setup() {
+        originalOut = System.out;
+        originalIn = System.in;
+    }
 
-        assertEquals("Inventory:\n" +
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+    }
+
+    private String getOriginalMenu() {
+        return "Menu:\n" +
+                "1,Caffe Americano,$3,30,true\n" +
+                "2,Caffe Latte,$2,55,true\n" +
+                "3,Caffe Mocha,$3,35,true\n" +
+                "4,Cappuccino,$2,90,true\n" +
+                "5,Coffee,$2,75,true\n" +
+                "6,Decaf Coffee,$2,75,true\n" +
+                "\n" +
+                "Your selection: ";
+    }
+
+    private String getOriginalInventory() {
+        return "Inventory:\n" +
                 "Cocoa,10\n" +
                 "Coffee,10\n" +
                 "Cream,10\n" +
@@ -21,17 +49,59 @@ class CoffeeMachineTest {
                 "Steamed Milk,10\n" +
                 "Sugar,10\n" +
                 "Whipped Cream,10\n" +
-                "\n" +
-                "Menu:\n" +
-                "1,Caffe Americano,$3,30,true\n" +
-                "2,Caffe Latte,$2,55,true\n" +
-                "3,Caffe Mocha,$3,35,true\n" +
-                "4,Cappuccino,$2,90,true\n" +
-                "5,Coffee,$2,75,true\n" +
-                "6,Decaf Coffee,$2,75,true\n" +
-                "\n" +
-                "Your selection: ", output);
+                "\n";
+    }
 
+    private String getAfterSel1Inventory() {
+        return dispensingAmericano() + "Inventory:\n" +
+                "Cocoa,10\n" +
+                "Coffee,10\n" +
+                "Cream,10\n" +
+                "Decaf Coffee,10\n" +
+                "Espresso,7\n" +
+                "Foamed Milk,10\n" +
+                "Steamed Milk,10\n" +
+                "Sugar,10\n" +
+                "Whipped Cream,10\n" +
+                "\n";
+    }
+
+    private String dispensingAmericano() {
+        return "Dispensing: Caffe Americano\n\n";
+    }
+
+
+    @Test
+    void quitting() {
+
+        var output = CoffeeMachineRunner.runCoffeeMachine("q\n");
+
+        assertEquals(getOriginalInventory() + getOriginalMenu(), output);
+
+    }
+
+    @Test
+    void refillAndQuit() {
+
+        var output = CoffeeMachineRunner.runCoffeeMachine("r\nq\n");
+
+        String originalInventory = getOriginalInventory();
+        String originalMenu = getOriginalMenu();
+
+        assertEquals(originalInventory + originalMenu + originalInventory + originalMenu,
+                output);
+    }
+
+    @Test
+    void selectAndQuit() {
+
+        var output = CoffeeMachineRunner.runCoffeeMachine("1\nq\n");
+
+        String originalInventory = getOriginalInventory();
+        String originalMenu = getOriginalMenu();
+
+        assertEquals(originalInventory + originalMenu + getAfterSel1Inventory() + originalMenu,
+                output);
     }
 
 }
